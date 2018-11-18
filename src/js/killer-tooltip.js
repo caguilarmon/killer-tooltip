90466163454625da60fc3fd06b21226a19527580
Carlos Mac <macbookpro@MacBook-Pro-de-MacBook.local>
2018-11-18 15:59:10 -0600

    Initial tooltip functionality added, including tooltip position options

--- src/js/killer-tooltip.js
+++ src/js/killer-tooltip.js
@@ -1,3 +1,64 @@
-const hola = 'hello';
+/**
+* Killer Tooltips - ES6/jQuery. (c) 2018 Carlos Aguilar Montoya
+* A library to create tooltips with custom settings.
+*
+*
+*
+*/
 
-console.log(hola);
+const tooltip = function(settings) {
+    const tooltips = $(settings.tooltipClass);
+    const tooltipPosition = settings.tooltipPosition;
+
+    const hideTooltip = (currentTooltip) => {
+      $(currentTooltip).children('.tooltipText').detach();
+    };
+
+    const showTooltip = (currentTooltip) => {
+      const tooltipData = getTooltipData(currentTooltip);
+      $(currentTooltip).append(`<span class="tooltipText">${tooltipData}</span>`);
+
+      switch (tooltipPosition) {
+        case 'right':
+          $(currentTooltip).children('.tooltipText').addClass('tooltipText--right');
+          break;
+        case 'left':
+          $(currentTooltip).children('.tooltipText').addClass('tooltipText--left');
+          break;
+        case 'top':
+          $(currentTooltip).children('.tooltipText').addClass('tooltipText--top');
+          break;
+        case 'bottom':
+          $(currentTooltip).children('.tooltipText').addClass('tooltipText--bottom');
+          break;
+        default:
+          $(currentTooltip).children('.tooltipText').addClass('tooltipText--right');
+
+      }
+    };
+
+    const getTooltipData = (currentTooltip) => {
+      return $(currentTooltip).data('text');
+    };
+
+    const onMouseLeave = () => {
+      $('.tooltip').mouseleave(function() {
+        let currentTooltip = $(this);
+        hideTooltip(currentTooltip);
+      });
+    };
+
+    const onMouseEnter = () => {
+      $('.tooltip').mouseenter(function() {
+        let currentTooltip = $(this);
+        showTooltip(currentTooltip);
+      });
+    };
+
+    const init = () => {
+      onMouseEnter();
+      onMouseLeave();
+    };
+
+    init();
+};
